c49e96fdc2bd75dede6cd77424d7f4a5634e53d6
Dhipa Poltak <dhipa2poltak@gmail.com>
2022-06-20 13:07:26 +0700

    adding unit test for SourceViewModel

---
+++ app/src/test/java/com/dpfht/testnews/viewmodel/SourceViewModelUnitTest.kt
@@ -0,0 +1,85 @@
+package com.dpfht.testnews.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.dpfht.testnews.MainCoroutineRule
+import com.dpfht.testnews.data.model.remote.Source
+import com.dpfht.testnews.data.repository.source.SourceRepository
+import com.dpfht.testnews.domain.model.GetSourceResult
+import com.dpfht.testnews.ui.source.SourceViewModel
+import com.dpfht.testnews.usecase.GetSourceUseCase
+import com.dpfht.testnews.usecase.GetSourceUseCaseImpl
+import com.dpfht.testnews.usecase.UseCaseResultWrapper
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.any
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+
+@RunWith(MockitoJUnitRunner::class)
+class SourceViewModelUnitTest {
+
+  @get:Rule
+  val taskExecutorRule = InstantTaskExecutorRule()
+
+  @get:Rule
+  val coroutineRule = MainCoroutineRule()
+
+  private lateinit var viewModel: SourceViewModel
+
+  @Mock
+  private lateinit var repository: SourceRepository
+
+  @Mock
+  private lateinit var getSourceUseCase: GetSourceUseCase
+
+  @Mock
+  private lateinit var sourceDataObserver: Observer<List<Source>>
+
+  @Before
+  fun setup() {
+    getSourceUseCase = GetSourceUseCaseImpl(repository)
+    viewModel = SourceViewModel(getSourceUseCase)
+  }
+
+  @Test
+  fun `fetch source successfully`() = runBlocking {
+    val source1 = Source(
+      id = "01",
+      name = "Test Source 1",
+      description = "desc source 1",
+      url = "https://test/source1",
+      category = "test",
+      language = "english",
+      country = "id"
+    )
+
+    val source2 = Source(
+      id = "02",
+      name = "Test Source 2",
+      description = "desc source 2",
+      url = "https://test/source2",
+      category = "test",
+      language = "english",
+      country = "id"
+    )
+
+    val sources = listOf(source1, source2)
+    val getSourceResult = GetSourceResult(sources)
+    val result = UseCaseResultWrapper.Success(getSourceResult)
+
+    whenever(getSourceUseCase.invoke(any())).thenReturn(result)
+
+    viewModel.sourceData.observeForever(sourceDataObserver)
+    viewModel.setCategoryName("test")
+    viewModel.start()
+
+    verify(sourceDataObserver).onChanged(eq(sources))
+  }
+}
